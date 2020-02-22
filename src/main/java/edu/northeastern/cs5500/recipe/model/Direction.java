6ed8d62b9c83e87da3ecabaea6cf645e07d03112
Henry Jiang <jianghanhua97@hotmail.com>
2020-02-22 11:19:28 -0800

    Added DirectionController class

--- src/main/java/edu/northeastern/cs5500/recipe/model/Direction.java
+++ src/main/java/edu/northeastern/cs5500/recipe/model/Direction.java
@@ -1,19 +1,24 @@
 package edu.northeastern.cs5500.recipe.model;
 
+import java.util.UUID;
+
 import lombok.Data;
 
 @Data
 public class Direction {
+    /** Define the enum for direction types */
     enum DirecitonTypes {
         PREP, WAIT, COOK;
     }
 
+    private UUID id;
+    private String name;
     // TODO: Ingredients
     // private List<Ingredient> ingredients;
     private int time;
-    private DirecitonTypes direcitonType;
     private int temp;
-    
+    private DirecitonTypes direcitonType;
+
 
     /**
      * Construct a User class
@@ -23,8 +28,60 @@ public class Direction {
 
     }
 
-    /** Get the unique id of the user. */
+    /** Get the unique id of the direction. */
+    public UUID getId() {
+        return this.id;
+    }
+
+    /** Get the name of the direction. */
+    public UUID getName() {
+        return this.name;
+    }
+
+    // TODO: Define getIngredients class
+    
     
 
+    /** Get the time of the direction. */
+    public int getTime() {
+        return this.time;
+    }
+
+    /** Get the temp of the direction. */
+    public int getTemp() {
+        return this.temp;
+    }
+
+    /** Get the temp of the direction. */
+    public DirecitonTypes getDirectionType() {
+        return this.direcitonType;
+    }
+
+
+    /** Set the id of the direction. */
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    /** Set the name of the direction. */
+    public void setName(String name) {
+        this.name = name;
+    }
     
+    /** Set the time of the direction. */
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    /** Set the temperature of the direction. */
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
+    // TODO: Define setIngredients class
+
+    /** Set the unique id of the user. */
+    public void setDirectionType(DirecitonTypes direcitonType) {
+        this.direcitonType = direcitonType;
+    }
 }
